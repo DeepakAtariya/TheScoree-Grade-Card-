06475f3dedd38f8b1a8116220ab10fdf0b450388
DeepakAtariya <Deepakkumaratariya@gmail.com>
2018-11-04 08:31:51 +0530

    angular - app component

--- backend-ignou/routes/api.php
+++ backend-ignou/routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is asigned the "api" middleware group. Enjoy building your API!
 |
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
