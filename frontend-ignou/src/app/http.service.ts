d7e6104cd5b9394f6fdb0feb050882091b11e0f8
teri guest <teri.guest@TERI.RES.IN>
2018-11-05 10:52:02 +0530

    initial commit

--- frontend-ignou/src/app/http.service.ts
+++ frontend-ignou/src/app/http.service.ts
@@ -4,6 +4,8 @@ import { Http } from '@angular/http';
 @Injectable()
 export class MyHttpService {
 
+  service = "it is on";
+
   constructor(private http: Http) {}
   getServices(serverPath) {
     return this.http.get(serverPath);
