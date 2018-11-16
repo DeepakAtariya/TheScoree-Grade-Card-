5c7bcbd45d489e7c29c52ee455ce6c3c3af3e2b9
DeepakAtariya <deepakkumaratariya@gmail.com>
2018-11-16 16:51:20 +0530

    routes : Progress

--- frontend-ignou/src/app/app.module.ts
+++ frontend-ignou/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,14 +11,20 @@ import { HeaderComponent } from './header/header.component';
 import { FieldsComponent } from './fields/fields.component';
 import { MiddleHeadingComponent } from './middle-heading/middle-heading.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SetupProfileComponent } from './setup-profile/setup-profile.component';
 
+const appRoutes : Routes = [
+  {path : '', component : AppComponent},
+  {path :'setupProfile'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FieldsComponent,
-    MiddleHeadingComponent
+    MiddleHeadingComponent,
+    SetupProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend-ignou/src/app/setup-profile/setup-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-profile',
+  templateUrl: './setup-profile.component.html',
+  styleUrls: ['./setup-profile.component.css']
+})
+export class SetupProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
