dd28dcd03b9960d9383d777dd0f3961da8c30512
DeepakAtariya <Deepakkumaratariya@gmail.com>
2019-03-28 22:10:24 +0530

    data is converted into json, card design is not ready

--- frontend-ignou/src/app/dashboard/dashboard.component.ts
+++ frontend-ignou/src/app/dashboard/dashboard.component.ts
@@ -18,6 +18,8 @@ export class DashboardComponent implements OnInit {
   // totalScore = [1,2,3,4,5,6,7,7,8,9,5,4,4,32,3,2];
   data: any;
   password: string;
+  data4small: any;
+  master_data = [];
   constructor(private route: Router, private dashboardService : DashboardService) { }
 
   ngOnInit() {
@@ -44,7 +46,47 @@ export class DashboardComponent implements OnInit {
     )
     .subscribe((data)=>{
       AppComponent.onShowLoader(0);
-      // console.log(data);
+      console.log(data['scores']);
+      this.data4small = data['scores'];
+      var scoreboard = {
+        'course_code' : '',
+        'Asgn1' : '',
+        'lab1' : '',
+        'lab2' : '',
+        'lab3' : '',
+        'lab4' : '',
+        'theory': '',
+        'status' : ''
+
+      }
+
+      // console.log(this.data4small);
+
+      for(var i=1; i<this.data4small.length; i++){
+            var scoreboard = {
+            'course_code' : '',
+            'Asgn1' : '',
+            'lab1' : '',
+            'lab2' : '',
+            'lab3' : '',
+            'lab4' : '',
+            'theory': '',
+            'status' : ''
+          }
+
+          scoreboard.course_code = this.data4small[i][0];
+          scoreboard.Asgn1 = this.data4small[i][1];
+          scoreboard.lab1 = this.data4small[i][2];
+          scoreboard.lab2 = this.data4small[i][3];
+          scoreboard.lab3 = this.data4small[i][4];
+          scoreboard.lab4 = this.data4small[i][5];
+          scoreboard.theory = this.data4small[i][6];
+          scoreboard.status = this.data4small[i][7];
+
+          this.master_data[i-1]=scoreboard;
+          // console.log(scoreboard);
+      }
+      console.log(this.master_data);
       this.scoreData = data['scores'];
       this.heading = this.scoreData[0];  
       // console.log(this.scoreData.slice(1,this.scoreData.length));
@@ -74,6 +116,8 @@ export class DashboardComponent implements OnInit {
 
       );
     });
+
+
 
   }
 
