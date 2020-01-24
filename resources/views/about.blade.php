325816938352e1272c354c75eeaee38c3486b355
deepakatariya <deepakkumaratariya@gmail.com>
2020-01-24 07:46:12 +0530

    fixed about and disclaimer

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -16,32 +16,11 @@
                   <tbody>
                     <tr>
                       <td class="standard_width">
-                      <hr>
-                        <p>Hello, Scoree is a general purpose website for the students of IGNOU. It shows term-end exam results as well as grade cards of IGNOU students in an improved structure. Scoree displays the programme name, total marks obtained in each course and the aggregate percentage. The students will be able to download their grade card in PDF format, or they can view their grade card on IGNOU’s official website. Currently it is able to show the results for a few programmes as mentioned in the Releases. More of the IGNOU programmes will be added in due course of time. <br><br>
-Students enrolled in various programmes will now be able to prepare for their term-end exams by referring to the notes provided by Scoree. The notes will be provided for every course and every unit, in a very comprehensive and exam-oriented format, thereby making it convenient for the students to understand the concepts and have it on their fingertips. This is currently available for MCA students; notes for other units/courses and programmes will be uploaded in due course of time.<br><br>
-Scoree saves your enrollment number and progress data to provide you with better services in the future. For more details, please read the Disclaimer and Privacy Policy.
-  </p>
-                        </td>
-                        <td>
-                          
-                        </td>
-                    </tr>
-                    <tr>
-                      <td> <h3>Releases</h3></td>
-                    </tr>
-                    <tr>
-                      <td>
-                      <p>
-                          <!-- <strong><span class="glyphicon glyphicon-info-sign"></span> Release 1.1 : </strong> Term-End Results are  -->
-                          <strong><span class="glyphicon glyphicon-info-sign"></span> Release 1.0 : </strong> With this release you can check the results of IGNOU BCA/MCA courses only.
-                        </p>
+                        <hr>
                         <p>
-                          <!-- <strong><span class="glyphicon glyphicon-info-sign"></span> Release 1.1 : </strong> Term-End Results are  -->
-                          <strong><span class="glyphicon glyphicon-info-sign"></span> Release 1.1 : </strong> MEG, BCOM Programs are added.
+                          Scoree is a technology for the students. It shows results of students in an improved structure. Scoree displays the programme name, total marks obtained in each course and the aggregate percentage. The students will be able to download their grade card in PDF format, or they can view their grade card on university official website. <br><br>Students enrolled in various programmes will now be able to prepare for their term-end exams by referring to the notes provided by Scoree. The notes will be provided for every course and every unit, in a very comprehensive and exam-oriented format, thereby making it convenient for the students to understand the concepts and have it on their fingertips.<br><br>Scoree saves your enrollment number and progress data to provide you with better services in the future. For more details, please read the Disclaimer and Privacy Policy.
                         </p>
-
-                        <p>There's a lot more to come, so stay tuned!</p>
-                      </td>
+                        </td>
                     </tr>
                    
                   </tbody> 
--- resources/views/disclaimer.blade.php
+++ resources/views/disclaimer.blade.php
@@ -16,7 +16,7 @@
                     <tr>
                       <td>
                         <hr>
-                        <p>The Scoree application is a free web application for the students of IGNOU. The information on this website is published for general information purpose only. The Scoree application does not guarantee the correctness, completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website <a id="mail_admin" href="https://www.thescoree.com">(https://www.thescoree.com)</a>, is strictly at your own risk. The Scoree application will not be liable for any losses and/or damages in connection with the use of our website. Whenever we update, amend or make any changes to this document, those changes will be prominently posted here.</p>
+                        <p>The Scoree application is a free web application for the students. The information on this website is published for general information purpose only. The Scoree application does not guarantee the correctness, completeness, reliability and accuracy of this information. Any action you take upon the information you find on this website <a id="mail_admin" href="https://www.thescoree.com">(https://www.thescoree.com)</a>, is strictly at your own risk. The Scoree application will not be liable for any losses and/or damages in connection with the use of our website. Whenever we update, amend or make any changes to this document, those changes will be prominently posted here.</p>
                       
                       </td>
                     </tr>
